4b58dc83621412d75c5522e92eb0bdcd2b9c86dd
Romain <romain.tastet@gmail.com>
2022-11-03 20:48:06 +0100

    PatchOpti2.3

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,11 @@
+/** Fonction récupération de l'ID */
 function getId() {
   let params = new URLSearchParams(document.location.search);
   return params.get("id");
 }
+getId();
 
+/** Affichage du produit */
 async function chargementProduit() {
   let r = await fetch(`http://localhost:3000/api/products/${getId()}`);
   r = await r.json();
@@ -27,3 +30,66 @@ async function chargementProduit() {
   }
 }
 chargementProduit();
+
+/** Fonction qui empêche l'utilisateur de rentrer des caractères innapropriés dans la quantité  */
+function nombresSeulement() {
+  let itemQuantityInput = document.querySelector("#quantity");
+  itemQuantityInput.setAttribute(
+    "onkeydown",
+    "return event.keyCode === 8 || event.keyCode === 46 ? true : !isNaN(Number(event.key))"
+  );
+}
+nombresSeulement();
+
+/** Fonction Stockage/Chargement Panier */
+
+let panier = [];
+
+function chargementPanier() {
+  panier = JSON.parse(localStorage.getItem("Panier"));
+}
+if (localStorage.getItem("Panier")) {
+  chargementPanier();
+}
+
+/** Fonction d'ajout au panier */
+const boutonPanier = document.querySelector("#addToCart");
+
+/**
+ * Crée un objet article
+ * @param {{qte: quantité, id: string, color: couleur }}
+ */
+
+boutonPanier.onclick = function () {
+  const infoArticle = {
+    qte: document.querySelector("#quantity").value,
+    id: getId(),
+    color: document.querySelector("#colors").value,
+  };
+  let existeDeja = false;
+  /** Si l'article est déjà présent avec même id et même couleur alors la quantité sera ajouter seulement */
+
+  if (infoArticle.qte > 0 && infoArticle.color != "") {
+    panier.forEach(function (article) {
+      if (
+        article.id === infoArticle.id &&
+        article.color === infoArticle.color
+      ) {
+        article.qte = Number(article.qte) + Number(infoArticle.qte);
+        existeDeja = true;
+      }
+    });
+
+    /** S'il est pas dans le panier il ajoute le contenu */
+
+    if (!existeDeja) {
+      panier.push(infoArticle);
+    }
+    /** On ajoute le contenu du panier au localstorage sous forme de Json */
+    localStorage.setItem("Panier", JSON.stringify(panier));
+    console.log(panier);
+  } else {
+    /** Sinon on retourne une erreur */
+    console.log("Vous devez choisir une quantité et une couleur !");
+  }
+};
